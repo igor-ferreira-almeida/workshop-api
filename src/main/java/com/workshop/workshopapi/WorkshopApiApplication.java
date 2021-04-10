7c85cb904be53c5d54d387b37123835e0a85b214
Igor Ferreira de Almeida <igor.rigel@gmail.com>
2021-04-10 16:13:22 -0300

    Initialize

---
+++ src/main/java/com/workshop/workshopapi/WorkshopApiApplication.java
@@ -0,0 +1,13 @@
+package com.workshop.workshopapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/workshop/workshopapi/WorkshopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workshop.workshopapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
